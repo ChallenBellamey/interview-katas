c266d072c26f843858dbf91a3458f4ddde7d4be6
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-15 15:49:03 +0100

    Added ResultsSection component.

--- simple-search-results-screen/src/App.js
+++ simple-search-results-screen/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import FiltersSection from './components/FiltersSection';
+import ResultsSection from './components/ResultsSection';
 
 function App() {
   return (
     <div className="App">
-    
+
     </div>
   );
 }
