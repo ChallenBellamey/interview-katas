a0ec71a4f671fd06504901efba9629a9b0a28278
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-15 15:46:35 +0100

    Added FiltersSection component.

--- simple-search-results-screen/src/App.js
+++ simple-search-results-screen/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import FiltersSection from './components/FiltersSection';
 
 function App() {
   return (
     <div className="App">
-
+    
     </div>
   );
 }
---
+++ simple-search-results-screen/src/components/FiltersSection.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class FiltersSection extends Component {
+
+}
+
+export default FiltersSection;
