468e04255bbdab34f94c01e246c2d83e66bd1421
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-16 16:35:57 +0100

    Added mockData file for development.

---
+++ simple-search-results-screen/src/data/mockData.js
@@ -0,0 +1,18 @@
+
+export default mockData = [
+    {
+      "name": "hotelone",
+      "starRating": 5,
+      "facilities": ["car park", "pool"]
+    },
+    {
+      "name": "hoteltwo",
+      "starRating": 3,
+      "facilities": ["car park", "gym"]
+    },
+    {
+      "name": "hotelthree",
+      "starRating": 3,
+      "facilities": []
+    }
+]
