cfdb551f2313da8c23dd0b6caea2954d8965baf5
Challen Bellamey <challenbellamey@hotmail.com>
2019-07-17 15:03:43 +0100

    Added getResults function which will return hotels from the mock database.

--- simple-search-results-screen/src/requests/get.js
+++ simple-search-results-screen/src/requests/get.js
@@ -13,3 +13,48 @@ export const getFacilitiesCount = async () => {
     }, {});
     return facilitiesCount;
 };
+
+export const getResults = async (filters) => {
+    let results;
+    if (filters) {
+        const { starRatings, facilities } = filters;
+        const filteredRatings = Object.keys(starRatings).reduce((acc, rating) => {
+            if (starRatings[rating]) {
+                acc.push(parseInt(rating));
+            };
+            return acc;
+        }, []);
+        const filteredFacilities = Object.keys(facilities).reduce((acc, facility) => {
+            if (facilities[facility]) {
+                acc.push(facility);
+            };
+            return acc;
+        }, []);
+        results = mockData.reduce((acc, hotel) => {
+            let match = true;
+            if (filteredRatings.length > 0) {
+                if (!filteredRatings.includes(hotel.starRating)) {
+                    match = false;
+                };
+            };
+            if (filteredFacilities.length > 0) {
+                const breakException = {};
+                try {filteredFacilities.forEach(facility => {
+                    if (!hotel.facilities.includes(facility)) {
+                        throw breakException;
+                    };
+                })}
+                catch (breakException) {
+                    match = false;
+                }
+            };
+            if (match) {
+                acc.push(hotel);
+            };
+            return acc;
+        }, []);
+    } else {
+        results = mockData;
+    };
+    return results;
+}
